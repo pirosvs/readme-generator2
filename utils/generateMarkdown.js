af304e0a5d5f1382f6b872cebcc6aa464f3a1631
darmstrong <armstrong2579@gmail.com>
2022-07-18 19:43:24 -0700

    add license link and badge info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (data.license == "none") {
+    return "";
+  } else if (data.license == "MIT") {
+    return `<a href="https://img.shields.io/badge/License-MIT-yellow.svg"></a>`;
+  } else if (data.license == "GNU GPLv3") {
+    return `<a href="https://img.shields.io/badge/License-GPLv3-blue.svg"></a>`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (data.license == "none") {
+    return "";
+  } else if (data.license == "MIT") {
+    return `<a href="https://opensource.org/licenses/MIT"></a>`;
+  } else if (data.license == "GNU GPLv3") {
+    return `<a href="https://www.gnu.org/licenses/gpl-3.0"></a>`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (data.license == "none") {
+    return "";
+  } else if (data.license == "MIT") {
+    return ;
+  } else if (data.license == "GNU GPLv3") {
+    return ;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -34,7 +58,7 @@ function generateMarkdown(data) {
   ${data.usage}.
 
   ## License
-  badge goes here or at top idk
+  This project is licensed under the ${data.license} license.
 
   ## Contritbuting
   To contribute to this project, please contact me by ${data.contribution}.
@@ -47,5 +71,8 @@ function generateMarkdown(data) {
 
   `;
 }
+
+// [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 module.exports = generateMarkdown;
